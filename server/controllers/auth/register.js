9b9428da3579a4b2f54fbdf13d34a31bef439019
Oleh Medvid <meandjob@protonmail.com>
2023-01-14 20:18:56 +0200

    add: images and public folder

--- server/controllers/auth/register.js
+++ server/controllers/auth/register.js
@@ -15,17 +15,14 @@ export const register = async (req, res) => {
   const passwordHash = await bcrypt.hash(password, salt);
 
   const newUser = new User({
-    firstName,
-    lastName,
-    email,
+    ...req.body,
     password: passwordHash,
-    picturePath,
-    friends,
-    location,
-    occupation,
     viewedProfile: Math.floor(Math.random() * 10000),
     impressions: Math.floor(Math.random() * 10000),
   });
   const savedUser = await newUser.save();
-  res.status(201).json(savedUser);
+    res.status(201).json({
+        ...savedUser,
+        password: null
+    });
 };
