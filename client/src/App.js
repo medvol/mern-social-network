b6daa1dc12d1421af492a22b8cab6c0fd75ae7ac
Oleh Medvid <meandjob@protonmail.com>
2023-01-18 01:12:35 +0200

    add: theme for MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 import { PagePoutes } from "components/PageRoutes/PagePoutes";
+import { selectMode } from "state/auth/selectors";
 
 function App() {
+  const mode = useSelector(selectMode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
         <Suspense fallback={null}>
-          <PagePoutes />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <PagePoutes />
+          </ThemeProvider>
         </Suspense>
       </BrowserRouter>
     </div>
