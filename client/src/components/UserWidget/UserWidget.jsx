91a36dab6474709a856ca7041c5844c842337794
Oleh Medvid <meandjob@protonmail.com>
2023-03-14 21:56:12 +0200

    add: userWidget to Homepage

--- client/src/components/UserWidget/UserWidget.jsx
+++ client/src/components/UserWidget/UserWidget.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   ManageAccountsOutlined,
@@ -10,27 +9,15 @@ import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage/UserImage";
 import { FlexBetween } from "components/FlexBetween/FlexBetween.styled";
 import WidgetWrapper from "components/WidgetWrapper/WidgetWrapper";
-import Loader from "components/Loader/Loader";
-import { getUser } from "services";
 
-const UserWidget = ({ userId, picturePath }) => {
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
+const UserWidget = ({ user }) => {
   const { palette } = useTheme();
   const navigate = useNavigate();
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
 
-  useEffect(() => {
-    setIsLoading(true)
-    const user = getUser(userId);
-    setUser(user);
-    setIsLoading(false)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   if (!user) {
-    setIsLoading(false)
     return null;
   }
 
@@ -42,107 +29,103 @@ const UserWidget = ({ userId, picturePath }) => {
     viewedProfile,
     impressions,
     friends,
+    _id,
+    picturePath,
   } = user;
 
   return (
-    <>
-      {isLoading && <Loader />}
-      <WidgetWrapper>
-        <FlexBetween
-          gap="0.5rem"
-          pb="1.1rem"
-          onClick={() => navigate(`/profile/${userId}`)}
-        >
+    <WidgetWrapper>
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${_id}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+        </FlexBetween>
+        <ManageAccountsOutlined />
+      </FlexBetween>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{occupation}</Typography>
+        </Box>
+      </Box>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <FlexBetween mb="0.5rem">
+          <Typography color={medium}>Who's viewed your profile</Typography>
+          <Typography color={main} fontWeight="500">
+            {viewedProfile}
+          </Typography>
+        </FlexBetween>
+        <FlexBetween>
+          <Typography color={medium}>Impressions of your post</Typography>
+          <Typography color={main} fontWeight="500">
+            {impressions}
+          </Typography>
+        </FlexBetween>
+      </Box>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Social Profiles
+        </Typography>
+
+        <FlexBetween gap="1rem" mb="0.5rem">
           <FlexBetween gap="1rem">
-            <UserImage image={picturePath} />
+            <img src="../assets/twitter.png" alt="twitter" />
             <Box>
-              <Typography
-                variant="h4"
-                color={dark}
-                fontWeight="500"
-                sx={{
-                  "&:hover": {
-                    color: palette.primary.light,
-                    cursor: "pointer",
-                  },
-                }}
-              >
-                {firstName} {lastName}
+              <Typography color={main} fontWeight="500">
+                Twitter
               </Typography>
-              <Typography color={medium}>{friends.length} friends</Typography>
+              <Typography color={medium}>Social Network</Typography>
             </Box>
           </FlexBetween>
-          <ManageAccountsOutlined />
+          <EditOutlined sx={{ color: main }} />
         </FlexBetween>
 
-        <Divider />
-
-        {/* SECOND ROW */}
-        <Box p="1rem 0">
-          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
-            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-            <Typography color={medium}>{location}</Typography>
-          </Box>
-          <Box display="flex" alignItems="center" gap="1rem">
-            <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
-            <Typography color={medium}>{occupation}</Typography>
-          </Box>
-        </Box>
-
-        <Divider />
-
-        {/* THIRD ROW */}
-        <Box p="1rem 0">
-          <FlexBetween mb="0.5rem">
-            <Typography color={medium}>Who's viewed your profile</Typography>
-            <Typography color={main} fontWeight="500">
-              {viewedProfile}
-            </Typography>
-          </FlexBetween>
-          <FlexBetween>
-            <Typography color={medium}>Impressions of your post</Typography>
-            <Typography color={main} fontWeight="500">
-              {impressions}
-            </Typography>
-          </FlexBetween>
-        </Box>
-
-        <Divider />
-
-        {/* FOURTH ROW */}
-        <Box p="1rem 0">
-          <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
-            Social Profiles
-          </Typography>
-
-          <FlexBetween gap="1rem" mb="0.5rem">
-            <FlexBetween gap="1rem">
-              <img src="../assets/twitter.png" alt="twitter" />
-              <Box>
-                <Typography color={main} fontWeight="500">
-                  Twitter
-                </Typography>
-                <Typography color={medium}>Social Network</Typography>
-              </Box>
-            </FlexBetween>
-            <EditOutlined sx={{ color: main }} />
-          </FlexBetween>
-
+        <FlexBetween gap="1rem">
           <FlexBetween gap="1rem">
-            <FlexBetween gap="1rem">
-              <img src="../assets/linkedin.png" alt="linkedin" />
-              <Box>
-                <Typography color={main} fontWeight="500">
-                  Linkedin
-                </Typography>
-                <Typography color={medium}>Network Platform</Typography>
-              </Box>
-            </FlexBetween>
-            <EditOutlined sx={{ color: main }} />
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Linkedin
+              </Typography>
+              <Typography color={medium}>Network Platform</Typography>
+            </Box>
           </FlexBetween>
-        </Box>
-      </WidgetWrapper>
-    </>
+          <EditOutlined sx={{ color: main }} />
+        </FlexBetween>
+      </Box>
+    </WidgetWrapper>
   );
 };
 
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { Navbar } from "components/Navbar/Navbar";
 import UserWidget from "components/UserWidget/UserWidget";
+import { useAuth } from "hooks/useAuth";
 
 export default function HomePage() {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const {
+    user
+  } = useAuth();
+
   return (
     <>
       <Box as="header">
         <Navbar />
       </Box>
-      <UserWidget />
+     <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget user={user} />
+        </Box>
+        </Box>
     </>
   );
 }
