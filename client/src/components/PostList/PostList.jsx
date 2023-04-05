351b5cab56743bffad1f5f0c001b94a0b6f42800
Oleh Medvid <meandjob@protonmail.com>
2023-04-06 00:32:52 +0300

    add: sortPosts component

--- client/src/components/PostList/PostList.jsx
+++ client/src/components/PostList/PostList.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import Post from "components/Post/Post";
 import Loader from "components/Loader/Loader";
+import SortPosts from "components/SortPosts/SortPosts";
 import { usePosts } from "hooks/usePosts";
 
 const PostList = () => {
   const { posts, isLoading, error } = usePosts();
+  const [filteredPosts, setfilteredPosts] = useState(posts);
+
+  useEffect(() => {
+    setfilteredPosts(posts);
+  }, [posts]);
+
+  const handleSort = (value) => {
+    const sortedPosts = [...filteredPosts].sort((a, b) => {
+      if (value === "Top") {
+        return (b.likes?.length ?? 0) - (a.likes?.length ?? 0);
+      } else {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      }
+    });
+    setfilteredPosts(sortedPosts);
+  };
 
   return (
     <>
@@ -14,10 +32,10 @@ const PostList = () => {
           Something went wrong... Please, try later.
         </Typography>
       )}
-
-      {!isLoading && posts && (
+      <SortPosts handleSort={handleSort} />
+      {!isLoading && filteredPosts.length > 0 && (
         <Box component="ul">
-          {posts.map((post) => (
+          {filteredPosts.map((post) => (
             <Post key={post._id} item={post} />
           ))}
         </Box>
---
+++ client/src/components/SortPosts/SortPosts.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  Box,
+  IconButton,
+  Tab,
+  Tabs,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+
+const options = ["Top", "Recent"];
+
+const SortPosts = ({ handleSort }) => {
+  const [sortBy, setSortBy] = useState("Recent");
+  const [showFilter, setShowFilter] = useState(false);
+
+  const { palette } = useTheme();
+
+  const handleTabChange = (event, value) => {
+    setSortBy(options[value]);
+    handleSort(options[value]);
+    setShowFilter(false);
+  };
+
+  return (
+    <Box sx={{ position: "relative", mt: "1rem" }}>
+      <Box textAlign="right">
+        <Typography
+          component="span"
+          sx={{ py: "0.5rem", mr: "0.25rem", color: palette.neutral.main }}
+        >
+          Sort By:
+        </Typography>
+        <Typography component="span">{sortBy}</Typography>
+        <IconButton variant="text" onClick={() => setShowFilter(!showFilter)}>
+          <ExpandCircleDownIcon />
+        </IconButton>
+      </Box>
+      {showFilter && (
+        <Tabs
+          orientation="vertical"
+          value={options.indexOf(sortBy)}
+          onChange={handleTabChange}
+          sx={{
+            position: "absolute",
+            width: "8rem",
+            right: 0,
+            top: "100%",
+            p: 0,
+            zIndex: 5,
+            borderRadius: "0.5rem",
+            backgroundColor: palette.neutral.light,
+          }}
+        >
+          <Tab
+            label="Top"
+            sx={{ minHeight: "2rem", p: 0, pr: "1rem", alignItems: "end" }}
+          />
+          <Tab
+            label="Recent"
+            sx={{ minHeight: "2rem", p: 0, pr: "1rem", alignItems: "end" }}
+          />
+        </Tabs>
+      )}
+    </Box>
+  );
+};
+
+export default SortPosts;
